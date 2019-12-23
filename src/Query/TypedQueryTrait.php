91d16d95dc1afef23218172eb5c4d41bac922b5d
orklah <ronan_morin@hotmail.com>
2019-12-23 14:26:11 +0100

    add phpdoc

--- src/Query/TypedQueryTrait.php
+++ src/Query/TypedQueryTrait.php
@@ -14,142 +14,130 @@ trait TypedQueryTrait
         return $this->query->getSQL();
     }
 
-
     public function getAST()
     {
         return $this->query->getAST();
     }
 
-
+    /** @return static */
     public function setQueryCacheDriver($queryCache)
     {
         $this->query->setQueryCacheDriver($queryCache);
         return $this;
     }
 
-
+    /** @return static */
     public function useQueryCache($bool)
     {
         $this->query->useQueryCache($bool);
         return $this;
     }
-
-
+    
     public function getQueryCacheDriver()
     {
         return $this->query->getQueryCacheDriver();
     }
 
-
+    /** @return static */
     public function setQueryCacheLifetime($timeToLive)
     {
         $this->query->setQueryCacheLifetime($timeToLive);
         return $this;
     }
-
-
+    
     public function getQueryCacheLifetime()
     {
         return $this->query->getQueryCacheLifetime();
     }
 
-
+    /** @return static */
     public function expireQueryCache($expire = true)
     {
         $this->query->expireQueryCache($expire);
         return $this;
     }
-
-
+    
     public function getExpireQueryCache()
     {
         return $this->query->getExpireQueryCache();
     }
-
-
+    
     public function free()
     {
         $this->query->free();
     }
 
-
+    /** @return static */
     public function setDQL($dqlQuery)
     {
         $this->query->setDQL($dqlQuery);
         return $this;
     }
-
-
+    
     public function getDQL()
     {
         return $this->query->getDQL();
     }
-
-
+    
     public function getState()
     {
         return $this->query->getState();
     }
-
-
+    
     public function contains($dql)
     {
         return $this->query->contains($dql);
     }
-
-
+    
     public function getFirstResult()
     {
         return $this->query->getFirstResult();
     }
-
-
+    
     public function getMaxResults()
     {
         return $this->query->getMaxResults();
     }
-
-
+    
     public function iterate($parameters = null, $hydrationMode = Query::HYDRATE_OBJECT)
     {
         return $this->query->iterate($parameters, $hydrationMode);
     }
 
-
+    /** @return static */
     public function setHint($name, $value)
     {
         $this->query->setHint($name, $value);
         return $this;
     }
 
-
+    /** @return static */
     public function setHydrationMode($hydrationMode)
     {
         $this->query->setHydrationMode($hydrationMode);
         return $this;
     }
 
-
+    /** @return static */
     public function setLockMode($lockMode)
     {
         $this->query->setLockMode($lockMode);
         return $this;
     }
-
-
+    
     public function getLockMode()
     {
         return $this->query->getLockMode();
     }
-    
-    
+
+    /** @return static */
     public function setFirstResult($firstResult)
     {
         $this->query->setFirstResult($firstResult);
         return $this;
     }
 
-
+    /** @return static */
     public function setMaxResults($maxResults)
     {
         $this->query->setMaxResults($maxResults);
